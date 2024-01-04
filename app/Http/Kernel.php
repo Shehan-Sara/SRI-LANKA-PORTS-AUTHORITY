705084c868f94b54acc2c81c4b8d15f0535ed819
Shehan Rashmika <116863196+Shehan-Sara@users.noreply.github.com>
2024-01-04 11:33:53 +0530

    Multiple role authentication created 🔥

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'admin' => \App\Http\Middleware\AdminMiddleware::class, //check registerd user
+        'action' => \App\Http\Middleware\CheckPermission::class, //check register user $$ user is a admin
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,9 @@ Route::get('/login', function () {
     return view('admin.login');
 });
 
-Route::get('/addrecord', function () {
-    return view('admin.insert');
-})->name('AdminInsert');
-
-
 Route::post('/varify', [UserController::class, 'login'])->name('login');
 Route::get('/logout', [UserController::class, 'logout'])->name('logout');
 
-Route::get('/newuser', [UserController::class, 'adduser'])->name('adduser');
-Route::post('/usercreate', [UserController::class, 'usercreate'])->name('usercreate');
 
 
 Route::middleware(['admin'])->group(function () {
@@ -43,8 +36,8 @@ Route::middleware(['admin'])->group(function () {
     Route::get('/otherpurchase', [TenderController::class, 'viewOther'])->name('AdminOther');
 
     // Tender record manageent
-    Route::get('/recorddelete/{id}', [TenderController::class, 'deleteRecord'])->name('DeleteRecord');
     Route::post('/insertdata', [TenderController::class, 'addrecord'])->name('AddRecord');
+    Route::post('/insertdata', [TenderController::class, 'newrecord'])->name('AdminInsert');
 
     // User Management 
     Route::get('/users', [UserController::class, 'viewuser'])->name('AdminUsers');
@@ -61,4 +54,11 @@ Route::middleware(['admin'])->group(function () {
         return view('admin.massages');
     })->name('AdminMassages');
 
+});
+
+Route::middleware(['action'])->group(function () {
+    Route::get('/recorddelete/{id}', [TenderController::class, 'deleteRecord'])->name('DeleteRecord');
+
+    Route::get('/newuser', [UserController::class, 'adduser'])->name('adduser');
+    Route::post('/usercreate', [UserController::class, 'usercreate'])->name('usercreate');
 });
