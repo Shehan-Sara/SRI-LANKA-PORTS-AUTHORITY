c4122b6eba4e902da8ab4b1a948a8e51a1dc59b6
Shehan Rashmika <116863196+Shehan-Sara@users.noreply.github.com>
2024-01-10 09:23:08 +0530

    few route changes 🚑️

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 })->name('Welcome');
 
 Route::get('/login', function () {
