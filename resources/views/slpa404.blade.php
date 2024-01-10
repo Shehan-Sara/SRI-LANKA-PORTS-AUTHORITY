373ee6c371dd29a000a502c0f3af96c55577b4b6
HashLenox <hslenox0419@gmail.com>
2024-01-10 17:26:54 +0530

    404 page updated

--- resources/views/slpa404.blade.php
+++ resources/views/slpa404.blade.php
@@ -4,65 +4,61 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="{{ asset('assets/css/404.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.icons.min.css') }}">
+    <title>404</title>
 
-    <link rel="stylesheet" href="{{ asset('assets/css/404.css') }}" />
-
-
-    <title>SPLA 404</title>
 </head>
 
 <body>
     <header class="header">
         <nav class="nav container">
+            <a href="#" class="nav-logo"> <img src="{{ asset('assets/img/logo.png') }}" alt=""> </a>
 
-            <img class="img-fluid rounded my-4 my-4" loading="lazy" src="{{ asset('assets/img/logo.png') }}"
-                width="245" height="80" alt="Logo" />
-
-
-            <div class="nav__menu" id="nav-menu">
-                <ul class="nav__list">
-                    <li class="nav__item">
-                        <a href="#" class="nav__link">Home</a>
+            <div class="nav-menu" id="nav-menu">
+                <ul class="nav-list">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Home</a>
                     </li>
-                    <li class="nav__item">
-                        <a href="#" class="nav__link">About</a>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">About</a>
                     </li>
-                    <li class="nav__item">
-                        <a href="#" class="nav__link">Contact</a>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Contact</a>
                     </li>
                 </ul>
 
-                <div class="nav__close" id="nav-close">
-                    <i class="bx bx-x"></i>
+                <div class="nav-close" id="nav-close">
+                    <i class="bi bi-x"></i>
                 </div>
             </div>
 
-            <div class="nav__toggle" id="nav-toggle">
-                <i class="bx bx-grid-alt"></i>
+            <div class="nav-toggle" id="nav-toggle">
+                <i class="bi bi-grid"></i>
             </div>
         </nav>
     </header>
 
     <main class="main">
         <section class="home">
-            <div class="home__container container">
-                <div class="home__data">
-                    <span class="home__subtitle">Error 404</span>
-                    <h1 class="home__title">Hey Buddy</h1>
-                    <p class="home__description">
+            <div class="home-container container">
+                <div class="home-data">
+                    <span class="home-subtitle">Ooops!</span>
+                    <h1 class="home-title">Error 404</h1>
+                    <p class="home-description">
                         We can't seem to find the page <br />
                         you are looking for.
                     </p>
-                    <a href="#" class="home__button"> Go Home </a>
+                    <a href="#" class="home-button"> Go Home </a>
                 </div>
 
-                <div class="home__img">
-                    <img src="assets/img/ghost-img2.png" alt="" />
-                    <div class="home__shadow"></div>
+                <div class="home-img">
+                    <img src="{{ asset('assets/img/ship-shrink.png') }}" alt="" />
+                    <div class="home-shadow"></div>
                 </div>
             </div>
 
-            <footer class="home__footer">
+            <footer class="home-footer">
                 <span>+94 112565892</span>
                 <span>|</span>
                 <span>slpa@example.com</span>
@@ -71,8 +67,55 @@
     </main>
 
     <script src="{{ asset('assets/js/scrollreveal.min.js') }}"></script>
-    <script src="{{ asset('assets/js/app.js') }}"></script>
 
+
+
+    <script>
+        const navMenu = document.getElementById('nav-menu'),
+            navToggle = document.getElementById('nav-toggle'),
+            navClose = document.getElementById('nav-close')
+
+
+        if (navToggle) {
+            navToggle.addEventListener('click', () => {
+                navMenu.classList.add('show-menu')
+            })
+        }
+
+        if (navClose) {
+            navClose.addEventListener('click', () => {
+                navMenu.classList.remove('show-menu')
+            })
+        }
+
+        const navLink = document.querySelectorAll('.nav-link')
+
+        function linkAction() {
+            const navMenu = document.getElementById('nav-menu')
+
+            navMenu.classList.remove('show-menu')
+        }
+        navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+        const sr = ScrollReveal({
+            distance: '90px',
+            duration: 3000,
+        })
+
+        sr.reveal(`.home-img`, {
+            origin: 'top',
+            delay: 400
+        })
+        sr.reveal(`.home-data`, {
+            origin: 'bottom',
+            delay: 600
+        })
+        sr.reveal(`.home-footer`, {
+            origin: 'bottom',
+            delay: 800
+        })
+    </script>
 </body>
 
 </html>
